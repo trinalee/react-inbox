44de277526299a9dc76d030aa73a1bd7e05db3f0
Lee <tleeg@allstate.com>
2017-09-04 22:36:01 -0500

    Unit 4 Checkpoint - implemented persisting toolbar button actions, compose message pending

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,24 +1,36 @@
 import React, {Component} from 'react'
 import Message from './Message'
 import Toolbars from './Toolbars'
-import messagesData from '../resources/messagesData'
 
 class Messages extends Component {
     state = {
-        messages: messagesData,
+        messages: [],
     }
 
-    // async componentDidMount() {
-    //     const response = await fetch('http://localhost:3001/api/messages')
-    //     const data = await response.json()
-    //     this.setState({
-    //         messages: data._embedded.messages
-    //     })
-    // }
+    async componentDidMount() {
+        const response = await fetch('/api/messages')
+        const data = await response.json()
+        this.setState({
+            messages: data._embedded.messages
+        })
+    }
 
-    toggleStar = (index) => {
+    toggleStar = async (index) => {
         let message = this.state.messages[index]
         message.starred = !this.state.messages[index].starred
+
+        await fetch('/api/messages', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                messageIds: [index + 1],
+                command: 'star',
+                star: message.starred
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
 
         this.setState(prevState => ({
             messages: [
@@ -42,61 +54,146 @@ class Messages extends Component {
         }))
     }
 
-    deleteSelected = () => {
+    deleteSelected = async () => {
+        await fetch('/api/messages', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                messageIds: this.state.messages.filter(message => message.selected)
+                    .map(msg => msg.id),
+                command: 'delete'
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+
         this.setState(prevState => ({
                 messages: prevState.messages.filter(message => message.selected ? false : true)
             }))
     }
 
-    markAsRead = () => {
+    markAsRead = async () => {
+        await fetch('/api/messages', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                messageIds: this.state.messages.filter(message => message.selected)
+                    .map(msg => msg.id),
+                command: 'read',
+                read: true
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+
         this.setState(prevState => ({
             messages: prevState.messages.map(message => {
-                let msg = message
+                let msg = message;
                 msg.read = message.selected ? true : message.read
+                msg.selected = false;
                 return msg
             })
         }))
     }
 
-    markAsUnread = () => {
+    markAsUnread = async () => {
+        await fetch('/api/messages', {
+            method: 'PATCH',
+            body: JSON.stringify({
+                messageIds: this.state.messages.filter(message => message.selected)
+                    .map(msg => msg.id),
+                command: 'read',
+                read: false
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+
         this.setState(prevState => ({
             messages: prevState.messages.map(message => {
-                let msg = message
+                let msg = message;
                 msg.read = message.selected ? false : message.read
+                msg.selected = false;
                 return msg
             })
         }))
     }
 
-    applyLabel = (labelName) => {
-        this.setState(prevState => ({
-            messages: prevState.messages.map(message => {
-                let msg = message
-                if (labelName !== "Apply label"
-                    && message.selected
-                    && !message.labels.some(label => (label === labelName))) {
-                    msg.labels.push(labelName)
-                }
-                return msg
-            })
-        }))
-    }
+    applyLabel = async (labelName) => {
+        if (labelName !== "Apply label") {
 
-    removeLabel = (labelName) => {
-        this.setState(prevState => ({
-            messages: prevState.messages.map(message => {
-                let msg = message
-                let labels
-                if (labelName !== "Remove label"
-                    && message.selected
-                    && message.labels.some(label => (label === labelName))) {
-                    labels = message.labels.filter(label => (label !== labelName))
-                    msg.labels = labels
+            console.log("messageIds ", this.state.messages.filter(message =>
+                (message.selected
+                && !message.labels.some(label => (label === labelName))))
+                .map(msg => msg.id))
+            console.log("labelName ", labelName)
+
+            await fetch('/api/messages', {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    messageIds: this.state.messages.filter(message =>
+                        (message.selected
+                        && !message.labels.some(label => (label === labelName))))
+                        .map(msg => msg.id),
+                    command: 'addLabel',
+                    label: 'dev'
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
                 }
-                return msg
-            })
-        }))
-    }
+            });
+
+            this.setState(prevState => ({
+                messages: prevState.messages.map(message => {
+                    let msg = message;
+                    if (message.selected
+                        && !message.labels.some(label => (label === labelName))) {
+                        msg.labels.push(labelName)
+                    }
+                    msg.selected = false;
+                    return msg
+                })
+            }))
+
+        }
+    };
+
+    removeLabel = async (labelName) => {
+        if (labelName !== "Remove label") {
+            await fetch('/api/messages', {
+                method: 'PATCH',
+                body: JSON.stringify({
+                    messageIds: this.state.messages.filter(message => message.selected)
+                        .map(msg => msg.id),
+                    command: 'removeLabel',
+                    label: labelName
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            });
+
+            this.setState(prevState => ({
+                messages: prevState.messages.map(message => {
+                    let msg = message;
+                    let labels;
+                    if (message.selected
+                        && message.labels.some(label => (label === labelName))) {
+                        labels = message.labels.filter(label => (label !== labelName))
+                        msg.labels = labels
+                    }
+                    msg.selected = false;
+                    return msg
+                })
+            }));
+        }
+    };
 
     toggleAll = () => {
         if (this.state.messages.every(message => message.selected === true)) {
@@ -116,7 +213,7 @@ class Messages extends Component {
                 })
             }))
         }
-    }
+    };
 
     render() {
         let unreadCount = this.state.messages ?
@@ -132,7 +229,7 @@ class Messages extends Component {
                 this.state.messages.every(message => message.selected === true) ? "all" :
                 this.state.messages.every(message => message.selected === false
                 || message.selected === undefined) ? "none" : "some"
-            : "none"
+            : "none";
 
         return (
             <div>
